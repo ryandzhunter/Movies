99ee073a66f05721b0d3fbc5e603bcafa8dc43f8
aryandi <aryandi2712@gmail.com>
2020-06-13 18:39:52 +0700

    add movies model

---
+++ app/src/main/java/com/kitabisa/movies/model/MovieResponse.kt
@@ -0,0 +1,67 @@
+package com.kitabisa.movies.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Aryandi Putra<aryandi2712@gmail.com> on 13/06/20.
+ */
+class MovieResponse(
+    @SerializedName("page")
+    @Expose
+    var page: Int? = null,
+    @SerializedName("total_results")
+    @Expose
+    var totalResults: Int? = null,
+    @SerializedName("total_pages")
+    @Expose
+    var totalPages: Int? = null,
+    @SerializedName("results")
+    @Expose
+    var results: List<MovieData>? = null
+)
+
+data class MovieData(
+    @SerializedName("vote_count")
+    @Expose
+    var voteCount: Int? = null,
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null,
+    @SerializedName("video")
+    @Expose
+    var video: Boolean? = null,
+    @SerializedName("vote_average")
+    @Expose
+    var voteAverage: Float? = null,
+    @SerializedName("title")
+    @Expose
+    var title: String? = null,
+    @SerializedName("popularity")
+    @Expose
+    var popularity: Float? = null,
+    @SerializedName("poster_path")
+    @Expose
+    var posterPath: String? = null,
+    @SerializedName("original_language")
+    @Expose
+    var originalLanguage: String? = null,
+    @SerializedName("original_title")
+    @Expose
+    var originalTitle: String? = null,
+    @SerializedName("genre_ids")
+    @Expose
+    var genreIds: List<Int>? = null,
+    @SerializedName("backdrop_path")
+    @Expose
+    var backdropPath: String? = null,
+    @SerializedName("adult")
+    @Expose
+    var adult: Boolean? = null,
+    @SerializedName("overview")
+    @Expose
+    var overview: String? = null,
+    @SerializedName("release_date")
+    @Expose
+    var releaseDate: String? = null,
+    var isFavorite: Boolean? = null)
--- app/src/main/java/com/kitabisa/movies/network/MovieApi.kt
+++ app/src/main/java/com/kitabisa/movies/network/MovieApi.kt
@@ -1,7 +1,15 @@
 package com.kitabisa.moviedb.network
 
+import com.kitabisa.movies.model.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 /**
  * Created by Aryandi Putra<aryandi2712@gmail.com> on 13/06/20.
  */
 interface MovieApi {
+
+    @GET("discover/movie")
+    suspend fun getMovies(): Response<MovieResponse>
+
 }
